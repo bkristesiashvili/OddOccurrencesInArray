f77c972f2ed066ec62636b4fa65acc10d533af62
Besik Kristesiashvili <besysfx@gmail.com>
2020-12-19 22:19:04 +0400

    Init Git

---
+++ OddOccurrencesInArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OddOccurrencesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sln = new Solution();
+            var array = new int[] { 8, 3, 8, 3, 8, 8, 9 };
+
+            Console.WriteLine($" Odd Occurred In Array: { sln.solution(array) }");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OddOccurrencesInArray/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OddOccurrencesInArray
+{
+    public class Solution
+    {
+        public int solution(int[] array)
+        {
+            try
+            {
+                if (array.Length == 0) return 0;
+                if (array.Length == 1) return array[^1];
+                if (array.Length % 2 == 0) return 0;
+
+                Array.Sort(array);
+
+                for (int i = 0; i < array.Length - 1; i += 2)
+                    if (array[i] != array[i + 1]) return array[i];
+
+                return array[^1];
+            }
+            catch { throw; }
+        }
+    }
+}
